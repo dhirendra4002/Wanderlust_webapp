7c9413790bbc7891f5f9c6702cbca1a2a8e6c65b
Dhirendra Kumar  Pandey <dhirendrap@interrait.com>
2025-09-24 18:48:17 +0530

    first commit

---
+++ terraform/ec2.tf
@@ -0,0 +1,136 @@
+/*resource "aws_key_pair" "deployer" {
+  key_name   = "terra-automate-key"
+  public_key = file("/Users/shubham/Documents/work/TrainWithShubham/terra-practice/terra-key.pub")
+}*/
+
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "allow_user_to_connect" {
+  name        = "allow TLS"
+  description = "Allow user to connect"
+  vpc_id      = aws_default_vpc.default.id
+  ingress {
+    description = "port 22 allow"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "port 80 allow"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "port 8080 allow"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Kubernetes Node Port"
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "for redis"
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SMTPS"
+    from_port   = 465
+    to_port     = 465
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  /*ingress {
+    description = "for apps"
+    from_port   = 3000
+    to_port     = 10000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }*/
+  ingress {
+    description = "SMTP"
+    from_port   = 25
+    to_port     = 25
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Kubernetes API ports"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "port 443 allow"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = " allow all outgoing traffic "
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "mysecurity"
+  }
+}
+
+resource "aws_instance" "testinstance" {
+  ami             = var.ami_id
+  instance_type   = var.instance_type
+  key_name        = "test_mumb"
+  security_groups = [aws_security_group.allow_user_to_connect.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update -y
+             sudo apt-get install docker.io -y
+sudo usermod -aG docker ubuntu && newgrp docker
+
+sudo apt update -y
+sudo apt install fontconfig openjdk-17-jre -y
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+  
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+  
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip
+unzip awscliv2.zip
+sudo ./aws/install
+
+              EOF
+  tags = {
+    Name = "Jenkins_master"
+  }
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+  }
+}
